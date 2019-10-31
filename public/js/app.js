48619d006dcb9e68097ee431e14426f2e8be7602
Deepak Kumar <dk1609008@gmail.com>
2019-10-31 11:20:47 +0530

    remove unnecessary console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,22 +1,3 @@
-console.log('Client side js in work');
-
-// fetch('http://puzzle.mead.io/puzzle').then((response) => {
-//     response.json().then((data) => {
-//         console.log(data);
-//     })
-// })
-
-// fetch('http://localhost:3000/weather?address=boston').then((response) => {
-//     response.json().then((data) => {
-//         if(data.error){
-//             console.log(data.error);
-//         }else{
-//             console.log(data.location);
-//             console.log(data.forecast);
-//         }
-//     })
-// })
-
 const weatherForm = document.querySelector('form');
 const input = document.querySelector('input');
 const msgOne = document.querySelector('#message1');
